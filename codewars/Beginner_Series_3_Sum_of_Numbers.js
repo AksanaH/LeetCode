f9d2f936426450728fe7d900815b8e13afe7524e
Aksana <aksa.hleb@gmail.com>
2025-09-22 22:46:18 -0500

    Solve codewars

---
+++ codewars/Beginner_Series_3_Sum_of_Numbers.js
@@ -0,0 +1,21 @@
+// Given two integers a and b, which can be positive or negative, find the sum 
+// of all the integers between and including them and return it. If the two 
+// numbers are equal return a or b.
+
+// Note: a and b are not ordered!
+
+function getSum(a, b) {
+    let sum = 0;
+    const min = Math.min(a, b);
+    const max = Math.max(a, b);
+    for (let i = min; i <= max; i++) {
+        sum += i
+    }
+    return sum
+}
+
+console.log(getSum(-1, 2));
+console.log(getSum(1, 3));
+console.log(getSum(3, 3));
+
+
