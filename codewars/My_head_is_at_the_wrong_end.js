2327fa90263b6d0843baaa1d8ad39f6c4cb0e139
Aksana <aksa.hleb@gmail.com>
2025-10-18 20:05:19 -0500

    Solve codewars problem

---
+++ codewars/My_head_is_at_the_wrong_end.js
@@ -0,0 +1,7 @@
+function fixTheMeerkat(arr) {
+    return arr.reverse();
+}
+
+console.log(fixTheMeerkat(["tail", "body", "head"]));
+console.log(fixTheMeerkat(["bottom", "middle", "top"]));
+console.log(fixTheMeerkat(["lower legs", "torso", "upper legs"]));
