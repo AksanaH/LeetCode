3027c7552bbb35dc8925e6be334394a023775db8
Aksana <aksa.hleb@gmail.com>
2026-01-22 21:41:11 -0600

    Solve codewars

---
+++ codewars/Remove_duplicates_from_list.js
@@ -0,0 +1,27 @@
+// Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
+
+// The order of the sequence has to stay the same.
+
+// Examples:
+
+// Input -> Output
+// [1, 1, 2] -> [1, 2]
+// [1, 2, 1, 1, 3, 2] -> [1, 2, 3]
+
+function removeDuplicates(arr) {
+    let result = [];
+    for (let num of arr) {
+        if (!result.includes(num)) {
+            result.push(num);
+        }
+    }
+    return result;
+}
+
+console.log(removeDuplicates([1, 2, 1, 1, 3, 2]));
+
+function removeDuplicates2(arr) {
+    return [...new Set(arr)];
+}
+
+console.log(removeDuplicates2([1, 2, 1, 1, 3, 2]));
